449b37b4c986899ec238eb3fb570484a1dee874b
ktonym <ktonym@gmail.com>
2017-12-08 20:42:03 +0300

    Update api.js

--- src/main/resources/static/src/api.js
+++ src/main/resources/static/src/api.js
@@ -7,7 +7,7 @@ export default {
         resetPasswordRequest: email =>
             axios.post("/auth/reset_password_request",email),//.then(res => res.data.message)
         validateToken: token =>
-            axios.post("/auth/validate_token",token).then(res => res.data),
+            axios.post("/auth/validate_token",{token}),//.then(res => res.data),
         changePassword: data =>
             axios.post("/auth/change_password",data)
     }
